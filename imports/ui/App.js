cdfa53b2bbef15b1798b67ba6dd688d84cf67310
Jatin Verma <jatin95oct@gmail.com>
2018-03-25 22:12:59 +0530

    appDone

--- imports/ui/App.js
+++ imports/ui/App.js
@@ -30,9 +30,17 @@ class App extends Component {
         if (this.state.hideCompleted) {
             filteredTasks = filteredTasks.filter(task => !task.checked);
         }
-        return filteredTasks.map((task) => (
-            <Task key={task._id} task={task}/>
-        ));
+        return filteredTasks.map((task) => {
+            const currentUserId = this.props.currentUser && this.props.currentUser._id;
+            const showPrivateButton = task.owner === currentUserId;
+
+            return (
+                <Task
+                    key={task._id}
+                    task={task}
+                    showPrivateButton={showPrivateButton}/>
+            );
+        });
     }
 
     handleSubmit(event) {
@@ -88,6 +96,7 @@ class App extends Component {
 }
 
 export default withTracker(() => {
+    Meteor.subscribe('tasks');
     return {
         tasks: Tasks.find({}, {sort: {createdAt: -1}}).fetch(),
         incompleteCount: Tasks.find({checked: {$ne: true}}).count(),
